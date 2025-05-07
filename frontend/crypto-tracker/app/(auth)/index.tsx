b4587174e07bd9b7dcfb389bcf7048cfbb0d382d
taystein435 <taystein435@gmail.com>
2025-05-07 03:46:47 +0100

    feat: update onboarding image style and text layout for improved aesthetics

--- frontend/crypto-tracker/app/(auth)/index.tsx
+++ frontend/crypto-tracker/app/(auth)/index.tsx
@@ -11,13 +11,13 @@ const Onboarding= () => {
     <SafeAreaView className="flex-1 justify-center items-center bg-white">
       <Image
         className=""
-        style={{ width:width*0.9, height:height * 0.5 ,borderRadius:10}}
+        style={{ width:width*0.9, height:height * 0.4 ,borderRadius:20, marginTop:height*0.1}}
 source="https://images.unsplash.com/photo-1645731504331-72636399448e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg0fHxjcnlwdG98ZW58MHx8MHx8fDA%3D"
         contentFit="cover"
         transition={1000}
       />
 
-      <Text className="font-bold text-black text-2xl mt-10 text-center">
+      <Text className="font-bold text-black text-2xl mt-16 px-4 text-center">
     Your one-stop solution for securing and managing your crypto assets
       </Text>
 
