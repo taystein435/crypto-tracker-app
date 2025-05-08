e7751e621016a39e3c7fde31b7045b464b39f23f
taystein435 <taystein435@gmail.com>
2025-05-08 18:24:27 +0100

    feat: enhance home screen layout by refining button arrangement and adding labels for actions

--- frontend/crypto-tracker/app/(tabs)/index.tsx
+++ frontend/crypto-tracker/app/(tabs)/index.tsx
@@ -10,62 +10,96 @@ const home = () => {
   return (
     <View className="flex-1 ">
       <SafeAreaView className="flex-1 bg-blue-400">
-        <View className="flex-row justify-between items-center gap-42 p-4 ">
-          <View className="flex-row ">
-            <Text className="text-white text=3xl">Taiwo Babatunde</Text>
+        <View className="flex-row justify-between items-center gap-42 p-3 ">
+          <View className="flex-row gap-2 items-center ">
+            <Text className="text-white text-3xl">Taiwo Babatunde</Text>
             <FontAwesome name="caret-down" size={24} color="white" />
-                 
           </View>
-          <View className="flex-row">
-          <TouchableOpacity className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg " style={{ borderRadius: 50, height: 50, width: 50, alignItems: "center", justifyContent: "center" }}>
-          <MaterialCommunityIcons name="qrcode" size={24} color="white" />
-              </TouchableOpacity>        
-              <TouchableOpacity className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg " style={{ borderRadius: 50, height: 50, width: 50, alignItems: "center", justifyContent: "center" }}>
-      
+          <View className="flex-row gap-2">
+            <TouchableOpacity
+              className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg "
+              style={{
+                borderRadius: 50,
+                height: 50,
+                width: 50,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <MaterialCommunityIcons name="qrcode" size={24} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg "
+              style={{
+                borderRadius: 50,
+                height: 50,
+                width: 50,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <MaterialCommunityIcons
-              name="bell-badge-outline"
-              size={24}
-              color="white"
-            />
-              </TouchableOpacity>              
-
+                name="bell-badge-outline"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
           </View>
         </View>
         <View>
-          <TouchableOpacity>
-            <Text></Text>
+          <TouchableOpacity className="flex-row mt-16 justify-between items-center gap-2 p-4 bg-white/10 backdrop-blur-md border border-white/20 rounded-full shadow-lg mx-4">
+            <Text className="text-white text-5xl font-bold">£100000 </Text>
             <FontAwesome name="caret-down" size={24} color="white" />
           </TouchableOpacity>
-          <Text></Text>
-          <Text></Text>
-          <View className="flex-row justify-between items-center gap-y-36 p-4 ">
-            <View>
-            <TouchableOpacity className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg " style={{ borderRadius: 50, height: 50, width: 50, alignItems: "center", justifyContent: "center" }}>
-              <Feather name="arrow-up-left" size={24} color="white" />
-            
-            </TouchableOpacity>
-            <Text className="text-white">Send</Text>
+          <View className="flex-row justify-between items-center gap-y-36 mt-10 p-4 ">
+            <View className="items-center">
+              <TouchableOpacity
+                className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg "
+                style={{
+                  borderRadius: 50,
+                  height: 50,
+                  width: 50,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Feather name="arrow-up-left" size={24} color="white" />
+              </TouchableOpacity>
+              <Text className="text-white">Send</Text>
             </View>
 
-           <View>
-           <TouchableOpacity className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg " style={{ borderRadius: 50, height: 50, width: 50, alignItems: "center", justifyContent: "center" }}>
-              <FontAwesome5 name="plus" size={24} color="white" />
-             
-            </TouchableOpacity>
-            <Text className="text-white">Buy</Text>
-           </View>
-          <View>
-          <TouchableOpacity className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg " style={{ borderRadius: 50, height: 50, width: 50, alignItems: "center", justifyContent: "center" }}>
-              <Feather name="arrow-up-right" size={24} color="white" />
-             
-            </TouchableOpacity>
-            <Text className="text-white">Receive</Text>
+            <View className="items-center">
+              <TouchableOpacity
+                className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg "
+                style={{
+                  borderRadius: 50,
+                  height: 50,
+                  width: 50,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <FontAwesome5 name="plus" size={24} color="white" />
+              </TouchableOpacity>
+              <Text className="text-white">Buy</Text>
             </View>
-           
-            
+            <View className="items-center">
+              <TouchableOpacity
+                className="bg-white/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg "
+                style={{
+                  borderRadius: 50,
+                  height: 50,
+                  width: 50,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Feather name="arrow-up-right" size={24} color="white" />
+              </TouchableOpacity>
+              <Text className="text-white">Receive</Text>
+            </View>
           </View>
-          
-          </View>
+        </View>
       </SafeAreaView>
       <View className="flex-1 rounded-tl-md rounded-tr-md bg-white"></View>
     </View>
